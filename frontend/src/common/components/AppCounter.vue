fe945dcf4b9a0da83ca3c84bf43e7e70ff13d29b
not-creative-man <lebedev_i_s@inbox.ru>
2025-11-26 21:38:40 +0300

    3.5 styles refactor and new AppCounter component

---
+++ frontend/src/common/components/AppCounter.vue
@@ -0,0 +1,189 @@
+<script setup>
+const props = defineProps({
+  count: {
+    type: Number,
+    required: true,
+  },
+});
+
+const emit = defineEmits(['increment', 'decrement']);
+</script>
+
+<template>
+  <div class="counter counter--orange ingredients__counter">
+    <button
+        type="button"
+        class="counter__button counter__button--minus"
+        :disabled="!count"
+        @click="emit('decrement')"
+    >
+      <span class="visually-hidden">Меньше</span>
+    </button>
+    <input
+        type="text"
+        name="counter"
+        class="counter__input"
+        :value="count"
+        readonly
+    />
+    <button
+        type="button"
+        class="counter__button counter__button--plus"
+        @click="emit('increment')"
+    >
+      <span class="visually-hidden">Больше</span>
+    </button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "@/assets/scss/ds-system/ds" as *;
+@use "@/assets/scss/ds-system/ds-typography" as *;
+@use "@/assets/scss/ds-system/ds-colors" as *;
+@use "@/assets/scss/ds-system/ds-shadows" as *;
+@use "@/assets/scss/mixins/mixins" as *;
+
+.counter {
+  display: flex;
+
+  justify-content: space-between;
+  align-items: center;
+}
+
+.counter__button {
+  $el: &;
+  $size_icon: 50%;
+
+  position: relative;
+
+  display: block;
+
+  width: 16px;
+  height: 16px;
+  margin: 0;
+  padding: 0;
+
+  cursor: pointer;
+  transition: 0.3s;
+
+  border: none;
+  border-radius: 50%;
+  outline: none;
+
+  &--minus {
+    background-color: $purple-100;
+
+    &::before {
+      @include p_center-all;
+
+      width: $size_icon;
+      height: 2px;
+
+      content: "";
+
+      border-radius: 2px;
+      background-color: $black;
+    }
+
+    &:hover:not(:active):not(:disabled) {
+      background-color: $purple-200;
+    }
+
+    &:active:not(:disabled) {
+      background-color: $purple-300;
+    }
+
+    &:focus:not(:disabled) {
+      box-shadow: $shadow-regular;
+    }
+
+    &:disabled {
+      cursor: default;
+
+      &::before {
+        opacity: 0.1;
+      }
+    }
+  }
+
+  &--plus {
+    background-color: $green-500;
+
+    &::before {
+      @include p_center-all;
+
+      width: $size_icon;
+      height: 2px;
+
+      content: "";
+
+      border-radius: 2px;
+      background-color: $white;
+    }
+
+    &::after {
+      @include p_center-all;
+
+      width: $size_icon;
+      height: 2px;
+
+      content: "";
+      transform: translate(-50%, -50%) rotate(90deg);
+
+      border-radius: 2px;
+      background-color: $white;
+    }
+
+    &:hover:not(:active):not(:disabled) {
+      background-color: $green-400;
+    }
+
+    &:active:not(:disabled) {
+      background-color: $green-600;
+    }
+
+    &:focus:not(:disabled) {
+      box-shadow: $shadow-regular;
+    }
+
+    &:disabled {
+      cursor: default;
+
+      opacity: 0.3;
+    }
+  }
+
+  &--orange {
+    background-color: $orange-200;
+
+    &:hover:not(:active):not(:disabled) {
+      background-color: $orange-100;
+    }
+
+    &:active:not(:disabled) {
+      background-color: $orange-300;
+    }
+  }
+}
+
+.counter__input {
+  @include r-s14-h16;
+
+  box-sizing: border-box;
+  width: 22px;
+  margin: 0;
+  padding: 0 3px;
+
+  text-align: center;
+
+  color: $black;
+  border: none;
+  border-radius: 10px;
+  outline: none;
+  background-color: transparent;
+
+  &:focus {
+    box-shadow: inset $shadow-regular;
+  }
+}
+</style>
--- frontend/src/modules/constructor/IngredientsSelector.vue
+++ frontend/src/modules/constructor/IngredientsSelector.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { defineProps } from "vue";
 import AppDrag from "@/common/components/AppDrag.vue";
+import AppCounter from "@/common/components/AppCounter.vue";
 
 const props = defineProps({
   modelValue: {
@@ -56,30 +57,7 @@ const decrementIngredient = (ingredient: string) => {
             ingredientsMap[ingredient]
           }}</span>
         </app-drag>
-        <div class="counter counter--orange ingredients__counter">
-          <button
-            type="button"
-            class="counter__button counter__button--minus"
-            :disabled="!ingredientCount(ingredient)"
-            @click="decrementIngredient(ingredient)"
-          >
-            <span class="visually-hidden">Меньше</span>
-          </button>
-          <input
-              type="text"
-              name="counter"
-              class="counter__input"
-              :value="ingredientCount(ingredient)"
-              readonly
-          />
-          <button
-              type="button"
-              class="counter__button counter__button--plus"
-              @click="incrementIngredient(ingredient)"
-          >
-            <span class="visually-hidden">Больше</span>
-          </button>
-        </div>
+        <app-counter :count="ingredientCount(ingredient)" @increment="incrementIngredient(ingredient)" @decrement="decrementIngredient(ingredient)" />
       </li>
     </ul>
   </div>
@@ -210,149 +188,5 @@ const decrementIngredient = (ingredient: string) => {
   width: 54px;
   margin-top: 10px;
   margin-left: 36px;
-}
-
-.counter {
-  display: flex;
-
-  justify-content: space-between;
-  align-items: center;
-}
-
-.counter__button {
-  $el: &;
-  $size_icon: 50%;
-
-  position: relative;
-
-  display: block;
-
-  width: 16px;
-  height: 16px;
-  margin: 0;
-  padding: 0;
-
-  cursor: pointer;
-  transition: 0.3s;
-
-  border: none;
-  border-radius: 50%;
-  outline: none;
-
-  &--minus {
-    background-color: $purple-100;
-
-    &::before {
-      @include p_center-all;
-
-      width: $size_icon;
-      height: 2px;
-
-      content: "";
-
-      border-radius: 2px;
-      background-color: $black;
-    }
-
-    &:hover:not(:active):not(:disabled) {
-      background-color: $purple-200;
-    }
-
-    &:active:not(:disabled) {
-      background-color: $purple-300;
-    }
-
-    &:focus:not(:disabled) {
-      box-shadow: $shadow-regular;
-    }
-
-    &:disabled {
-      cursor: default;
-
-      &::before {
-        opacity: 0.1;
-      }
-    }
-  }
-
-  &--plus {
-    background-color: $green-500;
-
-    &::before {
-      @include p_center-all;
-
-      width: $size_icon;
-      height: 2px;
-
-      content: "";
-
-      border-radius: 2px;
-      background-color: $white;
-    }
-
-    &::after {
-      @include p_center-all;
-
-      width: $size_icon;
-      height: 2px;
-
-      content: "";
-      transform: translate(-50%, -50%) rotate(90deg);
-
-      border-radius: 2px;
-      background-color: $white;
-    }
-
-    &:hover:not(:active):not(:disabled) {
-      background-color: $green-400;
-    }
-
-    &:active:not(:disabled) {
-      background-color: $green-600;
-    }
-
-    &:focus:not(:disabled) {
-      box-shadow: $shadow-regular;
-    }
-
-    &:disabled {
-      cursor: default;
-
-      opacity: 0.3;
-    }
-  }
-
-  &--orange {
-    background-color: $orange-200;
-
-    &:hover:not(:active):not(:disabled) {
-      background-color: $orange-100;
-    }
-
-    &:active:not(:disabled) {
-      background-color: $orange-300;
-    }
-  }
-}
-
-.counter__input {
-  @include r-s14-h16;
-
-  box-sizing: border-box;
-  width: 22px;
-  margin: 0;
-  padding: 0 3px;
-
-  text-align: center;
-
-  color: $black;
-  border: none;
-  border-radius: 10px;
-  outline: none;
-  background-color: transparent;
-
-  &:focus {
-    box-shadow: inset $shadow-regular;
-  }
 }
 </style>
