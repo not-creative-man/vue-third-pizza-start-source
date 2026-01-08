7ba7bd3ac6086e7233d55cfe0a8c41f56ccb8af4
not-creative-man <lebedev_i_s@inbox.ru>
2026-01-08 04:51:25 +0300

    4.7 added pinia and stores

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -5,4 +5,28 @@ export const routes = [
     component: () => import("@/views/HomeView.vue"),
     meta: { layout: "AppLayoutMain" },
   },
+  {
+    path: "/login",
+    name: "/login",
+    component: () => import("@/views/LoginView.vue"),
+    meta: { layout: "AppLayoutMain" },
+  },
+  {
+    path: "/cart",
+    name: "/cart",
+    component: () => import("@/views/CartView.vue"),
+    meta: { layout: "AppLayoutMain" },
+  },
+  {
+    path: "/orders",
+    name: "/orders",
+    component: () => import("@/views/OrdersView.vue"),
+    meta: { layout: "AppLayoutMain" },
+  },
+  {
+    path: "/profile",
+    name: "/profile",
+    component: () => import("@/views/ProfileView.vue"),
+    meta: { layout: "AppLayoutMain" },
+  },
 ];
--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -20,8 +20,8 @@
 
       <div class="user">
         <picture>
-          <source type="image/webp" srcset="img/users/user5@2x.webp 1x, img/users/user5@4x.webp 2x">
-          <img src="img/users/user5@2x.jpg" srcset="img/users/user5@4x.jpg" alt="Василий Ложкин" width="72" height="72">
+          <source type="image/webp" srcset="@/assets/img/users/user5@2x.webp 1x, @/assets/img/users/user5@4x.webp 2x">
+          <img src="@/assets/img/users/user5@2x.jpg" srcset="@/assets/img/users/user5@4x.jpg" alt="Василий Ложкин" width="72" height="72">
         </picture>
         <div class="user__name">
           <span>Василий Ложкин</span>
